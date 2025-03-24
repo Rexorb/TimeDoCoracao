c6ee0a99296664f2f91be136b19c5254baa30083
Roberto Henrique <roberto_sonicteam@hotmail.com>
2025-03-24 18:01:06 -0300

    Mesclando mudanças remotas

---
+++ Program.cs
@@ -0,0 +1,5 @@
+﻿Console.ForegroundColor = ConsoleColor.Red;
+Console.WriteLine("Time é o Vasco da Gama");
+Console.ForegroundColor = ConsoleColor.DarkRed;
+Console.WriteLine("O Flamengo é seleção");
+
